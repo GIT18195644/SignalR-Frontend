a35bfcdc66804522b10ca0e73ff0194f39e4a91d
GIT18195644 <dinukaindeewara@gmail.com>
2022-01-26 13:56:57 +0530

    Project Completed

---
+++ src/app/notification/notification.ts
@@ -0,0 +1,8 @@
+export class NotificationCountResult {
+    count: number | undefined;
+}
+
+export class NotificationResult {
+    employeeName: string | undefined;
+    tranType: string | undefined;
+} 
